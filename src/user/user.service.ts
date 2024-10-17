edbffc026336bfcdb60a967b19a1633d7c9c161c
Daniele Quintiliani <quintiliani.d@gmail.com>
2024-10-17 12:50:04 +0200

    added User and connected with db

---
+++ src/user/user.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+const select = {
+  id: true,
+  name: true,
+  email: true,
+  password: false,
+  role: true,
+  createdAt: true,
+  updatedAt: true,
+};
+@Injectable()
+export class UserService {
+  constructor(private service: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto): Promise<CreateUserDto> {
+    return this.service.user.create({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        password: createUserDto.password,
+      },
+    });
+  }
+
+  async findAll() {
+    return await this.service.user.findMany({ select });
+  }
+
+  findOne(id: number) {
+    return this.service.user.findUnique({
+      where: { id },
+      select,
+    });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return await this.service.user.update({
+      where: { id },
+      data: {
+        name: updateUserDto.name,
+        email: updateUserDto.email,
+        password: updateUserDto.password,
+      },
+    });
+  }
+
+  async remove(id: number) {
+    return await this.service.user.delete({
+      where: { id },
+    });
+  }
+}
